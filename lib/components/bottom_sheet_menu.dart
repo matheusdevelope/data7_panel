6d6c9d856194046e29fbce7f5b00e7bcf639c8f7
Matheus <emersonmatheustu@gmail.com>
2023-05-19 17:36:59 -0400

    ajust configs carousel to rows

--- lib/components/bottom_sheet_menu.dart
+++ lib/components/bottom_sheet_menu.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'dart:math' as math;
 import '../providers/theme_model.dart';
-import 'custom_slider.dart';
+import 'bottom_sheet_row.dart';
 
 class BottomSheetMenu extends StatelessWidget {
   const BottomSheetMenu({super.key});
@@ -12,28 +12,20 @@ class BottomSheetMenu extends StatelessWidget {
   final maxFont = 60.0;
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeModel>(builder: (ctx, ThemeModel theme, c) {
-      return
-          // ColoredBox(
-          //   color: Colors.transparent,
-          //   child:
-          Wrap(
-        crossAxisAlignment: WrapCrossAlignment.end,
-        alignment: WrapAlignment.spaceAround,
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          SizedBox(
-            child: Consumer<CarousselModel>(
-              builder: (context, CarousselModel caroussel, c) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
+    return Consumer<ThemeModel>(
+      builder: (ctx, ThemeModel theme, c) {
+        return Consumer<CarousselModel>(
+          builder: (context, CarousselModel caroussel, c) {
+            return Wrap(
+              crossAxisAlignment: WrapCrossAlignment.end,
+              alignment: WrapAlignment.spaceAround,
+              children: [
+                BottomSheetRow(
+                  child: [
                     Transform.rotate(
                       angle: 180 * math.pi / 180,
                       child: IconButton(
-                        padding: const EdgeInsets.all(0),
+                        padding: EdgeInsets.zero,
                         iconSize: theme.fontSizeMenuPanel + 8.0,
                         tooltip: 'Voltar',
                         onPressed: () {
@@ -45,89 +37,108 @@ class BottomSheetMenu extends StatelessWidget {
                         ),
                       ),
                     ),
-                    Expanded(child: Row()),
-                    Flexible(
-                      child: Wrap(
-                        alignment: WrapAlignment.center,
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        // crossAxisAlignment: CrossAxisAlignment.center,
-                        // mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          CustomIncrease(
-                            label: 'Duração',
-                            value: caroussel.autoPlayDuration.toDouble(),
-                            minValue: 1,
-                            maxValue: 60 * 10,
-                            onChange: (value) {
-                              caroussel.autoPlayDuration = value.toInt();
-                            },
-                            fontSize: theme.fontSizeMenuPanel,
-                          ),
-                          IconButton(
-                            iconSize: theme.fontSizeMenuPanel + 12,
-                            onPressed: () {
-                              caroussel.autoplay = !caroussel.autoplay;
-                            },
-                            color:
-                                caroussel.autoplay ? Colors.blue : Colors.black,
-                            tooltip: "AutoPlay Carrossel",
-                            icon: Icon(
-                              caroussel.autoplay
-                                  ? Icons.toggle_on
-                                  : Icons.toggle_off,
-                            ),
-                          ),
-                        ],
+                  ],
+                ),
+                BottomSheetRow(
+                  label: Text(
+                    "Auto Avançar Carrossel",
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: theme.fontSizeMenuPanel),
+                  ),
+                  child: [
+                    IconButton(
+                      padding: const EdgeInsets.only(right: 22),
+                      constraints: const BoxConstraints(),
+                      iconSize: theme.fontSizeMenuPanel + 12,
+                      onPressed: () {
+                        caroussel.autoplay = !caroussel.autoplay;
+                      },
+                      color: caroussel.autoplay ? Colors.blue : Colors.black,
+                      tooltip: "Ativo",
+                      icon: Icon(
+                        caroussel.autoplay ? Icons.toggle_on : Icons.toggle_off,
                       ),
                     ),
                   ],
-                );
-              },
-            ),
-          ),
-          const Divider(),
-          CustomIncrease(
-              label: 'Fonte Geral',
-              value: theme.fontSize,
-              minValue: minFont,
-              maxValue: maxFont,
-              onChange: (value) {
-                theme.fontSize = value;
-                theme.fontSizeTitlePanel = value;
-                theme.fontSizeDataPanel = value;
-                theme.fontSizeMenuPanel = value;
-              },
-              fontSize: theme.fontSizeMenuPanel),
-          CustomIncrease(
-              label: 'Titulo Painel',
-              value: theme.fontSizeTitlePanel,
-              minValue: minFont,
-              maxValue: maxFont,
-              onChange: (value) {
-                theme.fontSizeTitlePanel = value;
-              },
-              fontSize: theme.fontSizeMenuPanel),
-          CustomIncrease(
-              label: 'Linhas Painel',
-              value: theme.fontSizeDataPanel,
-              minValue: minFont,
-              maxValue: maxFont,
-              onChange: (value) {
-                theme.fontSizeDataPanel = value;
-              },
-              fontSize: theme.fontSizeMenuPanel),
-          CustomIncrease(
-              label: 'Menu Painel',
-              value: theme.fontSizeMenuPanel,
-              minValue: minFont,
-              maxValue: maxFont,
-              onChange: (value) {
-                theme.fontSizeMenuPanel = value;
-              },
-              fontSize: theme.fontSizeMenuPanel),
-        ],
-        // ),
-      );
-    });
+                ),
+                BottomSheetRow(
+                  label: Text(
+                    "Duração Carrossel",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: theme.fontSizeMenuPanel),
+                  ),
+                  child: [
+                    CustomIncrease(
+                      value: caroussel.autoPlayDuration.toDouble(),
+                      minValue: 1,
+                      maxValue: 60 * 10,
+                      onChange: (value) {
+                        caroussel.autoPlayDuration = value.toInt();
+                      },
+                      fontSize: theme.fontSizeMenuPanel,
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 4, right: 4, bottom: 4),
+                  child: CustomIncrease(
+                    label: 'Fonte Geral',
+                    value: theme.fontSize,
+                    minValue: minFont,
+                    maxValue: maxFont,
+                    onChange: (value) {
+                      theme.fontSize = value;
+                      theme.fontSizeTitlePanel = value;
+                      theme.fontSizeDataPanel = value;
+                      theme.fontSizeMenuPanel = value;
+                    },
+                    fontSize: theme.fontSizeMenuPanel,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 4, right: 4, bottom: 4),
+                  child: CustomIncrease(
+                      label: 'Titulo Painel',
+                      value: theme.fontSizeTitlePanel,
+                      minValue: minFont,
+                      maxValue: maxFont,
+                      onChange: (value) {
+                        theme.fontSizeTitlePanel = value;
+                      },
+                      fontSize: theme.fontSizeMenuPanel),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 4, right: 4, bottom: 4),
+                  child: CustomIncrease(
+                      label: 'Linhas Painel',
+                      value: theme.fontSizeDataPanel,
+                      minValue: minFont,
+                      maxValue: maxFont,
+                      onChange: (value) {
+                        theme.fontSizeDataPanel = value;
+                      },
+                      fontSize: theme.fontSizeMenuPanel),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 4, right: 4, bottom: 4),
+                  child: CustomIncrease(
+                      label: 'Menu Painel',
+                      value: theme.fontSizeMenuPanel,
+                      minValue: minFont,
+                      maxValue: maxFont,
+                      onChange: (value) {
+                        theme.fontSizeMenuPanel = value;
+                      },
+                      fontSize: theme.fontSizeMenuPanel),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
   }
 }
--- lib/components/custom_increase.dart
+++ lib/components/custom_increase.dart
@@ -11,7 +11,7 @@ class CustomIncrease extends StatefulWidget {
   final double fontSize;
   CustomIncrease({
     super.key,
-    required this.label,
+    this.label = '',
     required this.value,
     required this.minValue,
     required this.maxValue,
@@ -33,68 +33,89 @@ class _CustomIncreaseState extends State<CustomIncrease> {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(
-          widget.label,
-          style: TextStyle(fontSize: widget.fontSize),
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onLongPress: () => setState(() {
-                timer =
-                    Timer.periodic(const Duration(milliseconds: 60), (timer) {
-                  setState(() {
+        if (widget.label.isNotEmpty)
+          Text(
+            widget.label,
+            style: TextStyle(
+                fontSize: widget.fontSize, fontWeight: FontWeight.bold),
+          ),
+        Container(
+          // color: Colors.white,
+          padding: EdgeInsets.all(2),
+          // decoration: BoxDecoration(
+          //   border: Border.all(
+          //     color: Colors.grey.shade400,
+          //     style: BorderStyle.solid,
+          //     width: 0.5,
+          //   ),
+          //   color: Colors.white,
+          //   borderRadius: BorderRadius.circular(4),
+          // ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onLongPress: () => setState(() {
+                  timer =
+                      Timer.periodic(const Duration(milliseconds: 60), (timer) {
+                    setState(() {
+                      if (widget.value - 1 >= widget.minValue) {
+                        widget.onChange(widget.value - 1);
+                      }
+                    });
+                  });
+                }),
+                onLongPressEnd: (_) => setState(() {
+                  timer?.cancel();
+                }),
+                child: IconButton(
+                  padding: const EdgeInsets.only(right: 4),
+                  constraints: const BoxConstraints(),
+                  iconSize: widget.fontSize,
+                  onPressed: () {
                     if (widget.value - 1 >= widget.minValue) {
                       widget.onChange(widget.value - 1);
                     }
-                  });
-                });
-              }),
-              onLongPressEnd: (_) => setState(() {
-                timer?.cancel();
-              }),
-              child: IconButton(
-                iconSize: widget.fontSize,
-                onPressed: () {
-                  if (widget.value - 1 >= widget.minValue) {
-                    widget.onChange(widget.value - 1);
-                  }
-                },
-                icon: const Icon(Icons.remove),
+                  },
+                  icon: const Icon(Icons.remove),
+                ),
               ),
-            ),
-            Text(
-              widget.value.toString(),
-              style: TextStyle(fontSize: widget.fontSize),
-            ),
-            GestureDetector(
-              onLongPress: () => setState(() {
-                timer =
-                    Timer.periodic(const Duration(milliseconds: 60), (timer) {
-                  setState(() {
+              Text(
+                widget.value.toString(),
+                style: TextStyle(fontSize: widget.fontSize),
+              ),
+              GestureDetector(
+                onLongPress: () => setState(() {
+                  timer =
+                      Timer.periodic(const Duration(milliseconds: 60), (timer) {
+                    setState(() {
+                      if (widget.value + 1 <= widget.maxValue) {
+                        widget.onChange(widget.value + 1);
+                      }
+                    });
+                  });
+                }),
+                onLongPressEnd: (_) => setState(() {
+                  timer?.cancel();
+                }),
+                child: IconButton(
+                  padding: const EdgeInsets.only(
+                    left: 4,
+                  ),
+                  constraints: const BoxConstraints(),
+                  iconSize: widget.fontSize,
+                  onPressed: () {
                     if (widget.value + 1 <= widget.maxValue) {
                       widget.onChange(widget.value + 1);
                     }
-                  });
-                });
-              }),
-              onLongPressEnd: (_) => setState(() {
-                timer?.cancel();
-              }),
-              child: IconButton(
-                iconSize: widget.fontSize,
-                onPressed: () {
-                  if (widget.value + 1 <= widget.maxValue) {
-                    widget.onChange(widget.value + 1);
-                  }
-                },
-                icon: const Icon(Icons.add),
+                  },
+                  icon: const Icon(Icons.add),
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        )
       ],
     ));
   }
