44d7b947570acc07a93f71ec0262c3d63e85bc10
emersonmatheustu@gmail.com <emersonmatheustu@gmail.com>
2023-12-08 18:20:51 -0400

    turn settings model a singleton

--- lib/providers/Settings/settings_model.dart
+++ lib/providers/Settings/settings_model.dart
@@ -1,38 +1,33 @@
 import 'package:data7_panel/providers/Carousel/carousel_model.dart';
 import 'package:data7_panel/providers/Database/database_model.dart';
 import 'package:data7_panel/providers/Notification/notification_model.dart';
-// import 'package:data7_panel/providers/Panel/panel_model.dart';
 import 'package:data7_panel/providers/WindowsService/windows_service_model.dart';
 import 'package:data7_panel/providers/theme/theme_model.dart';
 
 class Settings {
-  late ThemeModel _theme;
-  late CarouselModel _carousel;
-  // late PanelSettings _panel;
-  late NotificationsSettings _notifications;
-  late DatabaseProvider _database;
-  late WindowsServiceProvider _windowsService;
+  static final Settings _instance = Settings._internal();
+
+  factory Settings() {
+    return _instance;
+  }
+
+  Settings._internal();
+
+  final ThemeModel _theme = ThemeModel();
+  final CarouselModel _carousel = CarouselModel();
+  final NotificationsSettings _notifications = NotificationsSettings();
+  final DatabaseProvider _database = DatabaseProvider();
+  final WindowsServiceProvider _windowsService = WindowsServiceProvider();
 
   ThemeModel get theme => _theme;
   CarouselModel get carousel => _carousel;
-  // PanelSettings get panel => _panel;
   NotificationsSettings get notifications => _notifications;
   DatabaseProvider get database => _database;
   WindowsServiceProvider get winService => _windowsService;
 
-  Settings() {
-    _theme = ThemeModel();
-    _carousel = CarouselModel();
-    // _panel = PanelSettings();
-    _notifications = NotificationsSettings();
-    _database = DatabaseProvider();
-    _windowsService = WindowsServiceProvider();
-  }
-
   Future<void> initialize() async {
     await _theme.initialize();
     await _carousel.initialize();
-    // await _panel.initialize();
     await _notifications.initialize();
     await _database.initialize();
     await _windowsService.initialize();
